b16f54fd18b2b461a1a123a3d610dc0ae802a5d4
Raleigh Dale <dalearaleigh@gmail.com>
2019-05-23 17:05:12 +0000

    image blur1

--- image_blur1.rb
+++ image_blur1.rb
@@ -6,20 +6,11 @@ class Image
   end
 
   def output_image
-
-    @new_string = ''
     @values.each do |x|
-
-      x.each do |cell|
-        @new_string += cell.to_s
+     puts x.join
       end
-
-      puts @new_string
-      @new_string = ''
     end
   end
-end
-
 
 image = Image.new([[0,0,0,0], [0,1,0,0], [0,0,0,1], [0,0,0,0]])
 
